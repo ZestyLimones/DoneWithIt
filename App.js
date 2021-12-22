f518d00677b5a6e98cea199ebb0f9878a6e06ca8
Micheal Limones <micheal@zestylimones.com>
2021-12-21 18:45:20 -0700

    added different components.

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableHighlight,
+  Button,
+  // Button,
+} from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log('text press');
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>Hello World!</Text>
+      <TouchableHighlight onPress={() => console.log('image tapped')}>
+        <Image
+          blurRadius={1}
+          source={{
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300',
+          }}
+        />
+      </TouchableHighlight>
+      <Button
+        title="Button!"
+        onPress={() => console.log('button clicked')}
+        color="green"
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
